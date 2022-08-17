7f4058443cf8d8081ec4783598f81b86e1289e12
kalunge <muthomititus+github@gmail.com>
2022-08-17 10:01:30 +0300

    Handle blur and focus events

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+  return (
+    <div>
+      <input
+        onChange={handleChange}
+        type="password"
+        placeholder="Enter Password"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
